00639c11931a9d1c3aa46d222398d788a37ef029
Nikolay <ziminnyr@mail.ru>
2025-09-27 00:29:44 +0300

    remove console-logs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,69 +9,59 @@ import { ERROR } from "./constants";
 import styled from "styled-components";
 
 const AppColumn = styled.div`
-	display: flex;
-	flex-direction: column;
-	justify-content: space-between;
-	width: 1000px;
-	min-height: 100%;
-	background-color: #fff;
-	margin: 0 auto;
-	position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 1000px;
+  min-height: 100%;
+  background-color: #fff;
+  margin: 0 auto;
+  position: relative;
 `;
 
 const Page = styled.div`
-	padding: 120px 0 20px;
+  padding: 120px 0 20px;
 `;
 
 export const Blog = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useLayoutEffect(() => {
-		const currentUserDataJSON = sessionStorage.getItem("userData");
+  useLayoutEffect(() => {
+    const currentUserDataJSON = sessionStorage.getItem("userData");
 
-		if (!currentUserDataJSON) {
-			return;
-		}
+    if (!currentUserDataJSON) {
+      return;
+    }
 
-		const currentUserData = JSON.parse(currentUserDataJSON);
+    const currentUserData = JSON.parse(currentUserDataJSON);
 
-		dispatch(
-			setUser({
-				...currentUserData,
-				roleId: Number(currentUserData.roleId),
-			}),
-		);
-	}, [dispatch]);
+    dispatch(
+      setUser({
+        ...currentUserData,
+        roleId: Number(currentUserData.roleId),
+      })
+    );
+  }, [dispatch]);
 
-	// useLayoutEffect(() => {
-	// 	fetch("http://localhost:3001/posts")
-	// 		.then((res) => res.json())
-	// 		.then(console.log)
-	// 		.catch((e) => console.log("ERROR: ", e));
-	// }, []);
-
-	return (
-		<>
-			<AppColumn>
-				<Header />
-				<Page>
-					<Routes>
-						<Route path="/" element={<Main />} />
-						<Route path="/login" element={<Autorization />} />
-						<Route path="/register" element={<Registration />} />
-						<Route path="/users" element={<Users />} />
-						<Route path="/post" element={<Post />} />
-						<Route path="/post/:id" element={<Post />} />
-						<Route path="/post/:id/edit" element={<Post />} />
-						<Route
-							path="*"
-							element={<Error error={ERROR.PAGE_NOT_EXIST} />}
-						/>
-					</Routes>
-				</Page>
-				<Footer />
-				<Modal />
-			</AppColumn>
-		</>
-	);
+  return (
+    <>
+      <AppColumn>
+        <Header />
+        <Page>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Autorization />} />
+            <Route path="/register" element={<Registration />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/post" element={<Post />} />
+            <Route path="/post/:id" element={<Post />} />
+            <Route path="/post/:id/edit" element={<Post />} />
+            <Route path="*" element={<Error error={ERROR.PAGE_NOT_EXIST} />} />
+          </Routes>
+        </Page>
+        <Footer />
+        <Modal />
+      </AppColumn>
+    </>
+  );
 };
